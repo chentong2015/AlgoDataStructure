79651867d87652e43af40c1fac61ceb51a622c55
Tong Chen <tong.chen@etu.utc.fr>
2021-05-12 18:36:01 +0200

    add hard array and strings part 1

---
+++ src/com/leetcode/top_interview_questions/hard_collection/HardArrayStrings2.java
@@ -0,0 +1,17 @@
+package com.leetcode.top_interview_questions.hard_collection;
+
+public class HardArrayStrings2 {
+
+    // TODO: First Missing Positive
+    // Given an unsorted integer array nums, find the smallest missing positive integer
+    // You must implement an algorithm that runs in O(n) time and uses constant extra space 不能使用排序，不能超过O(n)的时间复杂度
+    public int firstMissingPositive(int[] nums) {
+        // 测试理解：1. 如何确定第一个最小的正数? 忽略重复的值和负数的值 !!
+
+        // 6 9 2 3 1 -> 4
+
+        return 1;
+    }
+    
+
+}
