33ec3b165eb74f7f5c089a2e9139c3d896016974
Tong Chen <tong.chen@etu.utc.fr>
2021-06-22 17:48:03 +0200

    add ms test01

--- src/com/leetcode/code_test_interview/MicrosoftInterview.java
+++ src/com/leetcode/code_test_interview/MicrosoftInterview.java
@@ -1,9 +1,5 @@
 package com.leetcode.code_test_interview;
 
-import com.leetcode.top_interview_questions.base.TreeNode;
-
-import java.util.List;
-
 /**
  * 和平时工作一致，明确需求
  * 白板写代码，如何思考和解决问题
@@ -22,7 +18,32 @@ public class MicrosoftInterview {
     // http://microsoft-hire.me/pages/problems/leetcode.html
     // https://www.careercup.com/page?pid=microsoft-interview-questions
 
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        return null;
+    // Sort Colors
+    // Given an array nums with n objects colored red, white, or blue
+    // Sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue
+    // Integers 0, 1, and 2 to represent the color red, white, and blue, respectively
+    // nums = [2,0,2,1,1,0] -> [0,0,1,1,2,2]
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int left = 0;                // left位置的左边一定是0
+        int right = nums.length - 1; // right位置的右边一定是2
+        for (int index = 0; index <= right; index++) {
+            if (nums[index] == 0 && left < index) {
+                swap(nums, left, index);
+                index--;             // 交换过后需要退一步位置，判断交换过来的值 !!
+                left++;
+            }
+            if (nums[index] == 2) {
+                swap(nums, index, right);
+                index--;
+                right--;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
