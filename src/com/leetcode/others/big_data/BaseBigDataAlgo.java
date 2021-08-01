b9ffd4ee32fae6f6235cbb8f95b9f95a3e5263e0
Tong Chen <tong.chen@etu.utc.fr>
2021-08-01 21:48:27 +0200

    add new base big data algo

---
+++ src/com/leetcode/others/big_data/BaseBigDataAlgo.java
@@ -0,0 +1,18 @@
+package com.leetcode.others.big_data;
+
+// Dig Data 大数据金典算法
+public class BaseBigDataAlgo {
+
+    // 1. 随机均匀数据流采样问题
+
+    // 2. 基数估计(Cardinality Estimation)问题, 独立访客(Unique Visitor，简称UV)统计
+
+    // 3. 频率估计(Frequency Estimation)问题, 计算数据流中任意元素的频率
+
+    // 4. Heavy Hitters, 数据流中出现最频繁的k个元素(find top k frequent items in a data stream)
+
+    // 5. 范围查询(Range Query), 查询在某个范围内的元素出现的总次数
+
+    // 6. 成员查询(Membership Query)
+    //    定一个无限的数据流和一个有限集合，如何判断数据流中的元素是否在这个集合中
+}
