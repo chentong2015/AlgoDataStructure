fa6c9d6d817c986ad5c3ff788e8f4afb66d22a47
chen tong <chen2015tong@gmail.com>
2024-01-12 17:51:25 +0100

    refresh array dp programming

---
+++ src/data_structure/array_string/windows/StringSlidingWindow.java
@@ -0,0 +1,119 @@
+package data_structure.array_string.windows;
+
+// TODO: Sliding window technique 关于数组和字符串的滑动窗口算法
+// 1. [i, j]闭合的区间，用于"框选"一段数据的范围
+// 2. Consecutive 所选的区间内的数据必须是连续的
+// 3. while() 滑动窗口基于特定的判断条件，每次移动需要重新计算并更新条件
+public class StringSlidingWindow {
+
+    // Minimum Size Subarray Sum
+    // Given an array of positive integers nums and a positive integer target
+    // Return the minimal length of a "contiguous subarray" [nums l, nums l+1, ..., nums r-1, nums r]
+    // The sum is greater than or equal to target. If there is no such subarray, return 0
+    // nums = [2,3,1,2,4,3], target = 7  -> [4,3] -> 2
+    //
+    // O(2n)=O(n) O(1)
+    // 最终可能left和right从左到右各自移动一遍
+    public int minSubArrayLen(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        // 在中间while迭代的时候，不能返回，需要判断到数组的最后
+        while (right < nums.length) {
+            sum += nums[right];
+            // 如果累计的值超过了目标数值，则需要从左端进行循环
+            while (sum >= target) {  // 从头开始减少，通过[j-i]来缩小区间
+                minLength = Math.min(minLength, right - left + 1); // 保留移动过程中，有效最小窗口的宽度，注意长度 +1
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+
+    // Longest Substring
+    // Given a string s, find the length of the longest substring without repeating characters
+    // s consists of English letters, digits, symbols and spaces.
+    // s = "abcabcbb" -> "abc" -> 3
+    public int lengthOfLongestSubstring(String s) {
+        int left = 0;
+        int right = 0;
+        int result = 0;
+
+        // TODO. 将字符串中的char映射到int值，通过数组来统计指定char出现的次数
+        // int[256] for ASCII of 8 bits
+        int[] chars = new int[128];
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            chars[r]++;
+
+            // 如果指定的数组位置的值大于1, 说明之前有计算过，左边的lift坐标需要往右边移动
+            while (chars[r] > 1) {
+                char l = s.charAt(left);
+                chars[l]--;
+                left++;
+            }
+
+            // 在while循环之后，统计当前截取位置的距离长度
+            result = Math.max(result, right - left + 1);
+            right++;
+        }
+        return result;
+    }
+
+    // TODO: 滑动窗口算法的优化版本, left坐标位置不需要逐步移动，而是一步跳跃式的移动到指定位置
+    // "mabcdeafbdgcbb"
+    // a: 6    字符始终记录出现的最后一个位置的index，每次更新位置     chars[r] = right;
+    // b: 12   当发现字符出现，取出它的位置，并从它的下一个位置开始截断  left = index + 1;
+    // c: 11   同时每一步更新结果                                 res = Math.max(,)
+    // d: 9
+    // ...
+    public int lengthOfLongestSubstring2(String s) {
+        int left = 0;
+        int right = 0;
+        int res = 0;
+        // TODO: 这个字符数组中存放的是，某个字符在字符串中出现的最后位置
+        //  如果出现重复，则它前面的位置全部跳过，避免一步一步滑动
+        Integer[] chars = new Integer[128]; // char -> int 根据ASCII码值表对应十进制的值，作为数组的下标
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            Integer charLastIndex = chars[r]; // 使用引用类型来做null空的判断，避免之间判断index的范围
+            if (charLastIndex != null && left <= charLastIndex && charLastIndex < right) {
+                left = charLastIndex + 1;
+            }
+            res = Math.max(res, right - left + 1); // 需要在移动过程中记录结果
+            chars[r] = right;
+            right++;
+        }
+        return res;
+    }
+
+    // TODO: 从数据的连续性推导出"数学公式"的成立，假设能够拆分的数字个数是k个，连续数字的第一个数字是x
+    //  时间复杂度必须小于O(n)，一般只有两种可能O(log(n)) & O(n^0.5)和数据的平方有关系
+    // Consecutive Numbers Sum
+    // Given an integer n, return the number of ways you can write n as the sum of consecutive positive integers
+    // n = 5   -> 5 = 2 + 3 -> 2
+    // n = 9   -> 9 = 4 + 5 = 2 + 3 + 4 -> 3
+    // n = 15  -> 15 = 8 + 7 = 4 + 5 + 6 = 1 + 2 + 3 + 4 + 5 -> 4
+    // 1. 如何判断一个数字能拆，如何拆才是合理的? 如何统计拆分的可能性?
+    // 2. 反向推理，使用"Sliding Windows"从结果推理出Sum的总和值!! O(n) O(1)
+    // 3. 通项公式，转换成数学问题
+    //    x+(x+1)+(x+2)+(x+3)...(x+(k-1)) = N
+    //    ((2x+k-1)*k)/2 = N
+    //    xk + k(k-1)/2 = N
+    //    xk = N - k(k-1)/2    对每一个拆分出来的数目k，对k求余数必须是整除的(结果保证x的值)
+    //    1< k < Math.sprt(2N) 这里k的范围约束在n的平方根之下，降低时间复杂度为O(n^0.5)
+    public int consecutiveNumbersSum(int n) {
+        int count = 1;
+        for (int k = 2; k < Math.sqrt(2 * n); k++) {
+            if ((n - (k * (k - 1) / 2)) % k == 0) count++;
+        }
+        return count;
+    }
+}
---
+++ src/questions/greedy_dynamic_pro/array/DynamicProgrammingArray3.java
@@ -0,0 +1,60 @@
+package questions.greedy_dynamic_pro.array;
+
+public class DynamicProgrammingArray3 {
+
+    // TODO. DP Programming 在遍历数组的每一步，都需要附加计算当前结果(且该结果依赖于前一步保存的计算结果)
+    // 1. 数组中的最大值并不能决定划分的选择，以及值的取舍
+    // 2. 由于划分的子数组的长度不确定，因此无法适用于Sliding Window算法
+
+    // Partition Array for Maximum Sum
+    // Given an integer array arr, partition the array into (contiguous) subArrays of length at most k.
+    // After partitioning, each subArray has their values become the maximum value of that subArray.
+    //
+    // Input: arr = [1,15,7,9,2,5,10], k = 3  => Output: 84
+    // Explanation: [15,15,15,9,10,10,10]
+    // Input: arr = [1,4,1,5,7,3,6,1,9,9,3], k = 4  => Output: 83
+    // Explanation: [4,4,7,7,7,9,9,9,9,9,9] = 29 + 54 = 83
+    //
+    // 找指定长度内的最大值，并可以将其他位置的都变成它
+    // For j = 1 .. k that keeps everything in bounds
+    //  dp[i] is the maximum of dp[i-j] + max(A[i-1], ..., A[i-j]) * j
+    //
+    // 1 5 2  k=2
+    // 1 0 0
+    // -> 1 5 0
+    // -> 1 5*2 0 (1+5 < 5*2) 因此将1的位置设置成5会得出更大的值
+    // 1 10 12
+    public static int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length; // Length of the input array
+        int[] dp = new int[n]; // Initialize an array to store maximum sums
+        for (int i = 0; i < n; i++) {
+            int maxPartitionSum = Integer.MIN_VALUE; // Initialize the maximum sum for the current element
+            int maxElement = Integer.MIN_VALUE;      // Initialize the maximum element within the current group
+
+            // 从index位置往后划分(最大k个长度)，计算在这个过程中能够得出的最大值
+            // 如果往后划分的区间中有更大的值，则按照“更大的值*Length长度” + “之前记录的最大值” =得出index位置可以划分的最大值
+            for (int j = i; j >= (i - k + 1) && j >= 0; j--) {
+                int partitionLength = i - j + 1;           // Length of the current partition
+                maxElement = Math.max(maxElement, arr[j]); // Find the maximum element in the partition
+                int currentPartitionSum = maxElement * partitionLength; // Calculate the sum of the current group
+
+                if (j > 0) {
+                    int previousMaxSum = dp[j - 1];        // Maximum sum before the current partition
+                    maxPartitionSum = Math.max(maxPartitionSum, previousMaxSum + currentPartitionSum);
+                } else {
+                    maxPartitionSum = Math.max(maxPartitionSum, currentPartitionSum);
+                }
+            }
+
+            // 存储当前位置在任意长度划分之后，能够得出的最大值
+            // Store the maximum sum up to the current element
+            dp[i] = maxPartitionSum;
+        }
+        return dp[n - 1]; // Return the maximum sum after partitioning
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,5,2};
+        System.out.println(maxSumAfterPartitioning(arr, 2));
+    }
+}
