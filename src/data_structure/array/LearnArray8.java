6eed95d9b63a7a084b872eed79aab649f9fccd7c
chen tong <chen2015tong@gmail.com>
2024-02-04 14:24:06 +0100

    add learn array

--- src/data_structure/array/LearnArray8.java
+++ src/data_structure/array/LearnArray8.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class LearnArray8 {
 
+    // TODO. 直接利用数组并本身来对数据进行统计, 通过index区间统计
     // Minimum Rounds to Complete All Tasks
     // You are given a 0-indexed integer array tasks, where tasks[i] represents the difficulty level of a task.
     // In each round, you can complete either 2 or 3 tasks of the same difficulty level.
@@ -40,7 +41,6 @@ public class LearnArray8 {
         return minRounds;
     }
 
-    // TODO. 直接利用数组并本身来对数据进行统计, 通过index区间统计
     // O(n*logn)+O(n)  O(1)
     // 使用时间复杂度来换取空间复杂度
     public int minimumRounds2(int[] tasks) {
@@ -70,5 +70,33 @@ public class LearnArray8 {
             ans++;
         }
         return ans;
+    }
+
+    // TODO. 循环数组：从其中框选k段的长度, 保证截取的长度中包含开头或者结尾index
+    // Maximum Points You Can Obtain from Cards
+    // The points are given in the integer array cardPoints
+    // In one step, you can take one card from the beginning or from the end of the row.
+    // You have to take exactly k cards.
+    // Return the maximum score you can obtain.
+    //
+    // [1,2,3,4,5,6,1], k = 3 -> 1 + 6 + 5 = 12
+    // O(k) O(1) 只需要在数组中循环k个位置的值
+    public int maxScore(int[] cardPoints, int k) {
+        int baseScore = 0;
+        for (int index = 0; index <k; index++) {
+            baseScore += cardPoints[index];
+        }
+
+        int left = k - 1;
+        int index = 0;
+        int maxScore = baseScore;
+        while (index < k) {
+            // 循环数组：每移动一个位置，重新计算累计的总和
+            baseScore = baseScore - cardPoints[left] + cardPoints[cardPoints.length - 1 - index];
+            maxScore = Math.max(maxScore, baseScore);
+            left--;
+            index++;
+        }
+        return maxScore;
     }
 }
