b70c3be8bf155ef4aafb09caebaa68b8ad0e6da7
chen tong <chen2015tong@gmail.com>
2024-06-16 19:06:24 +0200

    update most frequency element

--- src/work_interview/alibaba/AlibabaQuestion.java
+++ src/work_interview/alibaba/AlibabaQuestion.java
@@ -1,42 +1,35 @@
 package work_interview.alibaba;
 
+import java.util.Arrays;
+
 public class AlibabaQuestion {
 
-    // Alibaba Interview Question
+    // TODO: 数据统计的相互抵消性质，一定是出现次数多的那个数据占优势(被留下来)
     // More than half frequency element
     // Find elements with more than half the frequency, suppose there is only one answer
     // nums = [1,2,2,3,3,2,6,2,2]  ->  2
-    // 1. 使用HashMap<>来存储每个元素的频率 O(n)       O(n)
-    // 2. 使用两遍遍历数组：第一遍找出其中出现次数最多的数字，第一遍统计这个数字出现的次数是否超过一半 !!
-
-    // TODO: 从特殊性到一般性的过渡，满足同一个逻辑条件的成立
-    //       数据统计的相互抵消性质，一定是出现次数多的那个数据占优势(被留下来) !!
-    // More than half frequency element
-    // Find elements with more than half the frequency, suppose there is only one answer
-    // nums = [1,2,2,3,3,2,6,2,2]  ->  2
-    // 2,2,2,2,2,1,3,3,6  假如有一个数字出现的次数超过一半，通过count统计最后留下来的mostFrequencyValue一定是这个数字
-    // 1,2,2,3,3,2,6,2,2  数据打乱之后依然能保留到这个数字，原因是因为在count统计的过程中数据会呈现相互抵消的效果 !!
-    public int findMostFrequencyElement2(int[] gifts, int n) {
-        if (gifts.length < n || gifts.length == 0) return 0;
+    // 2,2,2,2,2,1,3,3,6  假如有一个数字出现的次数超过一半，count统计的mostFrequencyValue一定是这个数字
+    // 1,2,2,3,3,2,6,2,2  统计的mostFrequencyValue值和数组中数据的排序无关
+    //
+    // O(n) O(1) 最佳的空间复杂度，避免使用HashMap<>开辟额外空间
+    public int findMostFrequencyElement(int[] gifts, int n) {
         int count = 0;
         int mostFrequencyValue = gifts[0];
         for (int i = 0; i < n; i++) {
             if (count == 0) {
-                // 只有当count被累积清空之后，才会变动记录的数字mostFrequencyValue
+                count = 1;   // 重新累计新的统计数字
                 mostFrequencyValue = gifts[i];
-                count = 1;
             } else {
-                // 累计频率或者是抵消
-                if (mostFrequencyValue == gifts[i]) count++;
-                else count--;
+                if (mostFrequencyValue == gifts[i]) {
+                    count++;
+                } else {
+                    count--; // 并新的数字抵消统计
+                }
             }
         }
-        // 前面统计出来的最大频率的数据，并没有准确的统计出其数目
-        // 需要再次统计最大频率的数目是否超过一半(该数据据不一定存在)
-        int size = 0;
-        for (int i = 0; i < n; i++) {
-            if (gifts[i] == mostFrequencyValue) size++;
-        }
+        // 最后确定元素的最大频率是否超过一半(该数据据不一定存在)
+        int element = mostFrequencyValue;
+        int size = (int) Arrays.stream(gifts).filter(item -> item == element).count();
         return (size > n / 2) ? mostFrequencyValue : 0;
     }
 }
